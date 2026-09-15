d060aa31a4cf2069d97295051cb7957022fbaab1
Sebastián Cárdenas <sebastian.cardenas2301@alumnos.ubiobio.cl>
2026-09-15 19:59:24 -0300

    se hace una prueba de thymeleaf

--- springboot-applications/src/main/java/com/proyectoubbconfig/iswspring/app1/springboot_applications/controllers/InicioController.java
+++ springboot-applications/src/main/java/com/proyectoubbconfig/iswspring/app1/springboot_applications/controllers/InicioController.java
@@ -1,14 +1,19 @@
 package com.proyectoubbconfig.iswspring.app1.springboot_applications.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // ¡Importante importar esto!
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InicioController {
 
     @GetMapping("/")
-    public String inicio() {
-        // Esto le dice a Spring que busque un archivo llamado "index.html" en la carpeta templates
-        return "index";
+    public String inicio(Model model) {
+        // "Empaquetamos" variables bajo un nombre clave
+        model.addAttribute("titulo", "Plataforma de Prácticas UBB");
+        model.addAttribute("mensajeBievenida", "¡El enlace con Thymeleaf funciona a la perfección!");
+        model.addAttribute("usuarioActivo", "Sebastián Cárdenas"); 
+        
+        return "index"; // Sigue apuntando a index.html
     }
 }
